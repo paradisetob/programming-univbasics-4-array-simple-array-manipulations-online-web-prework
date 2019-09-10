e9a46d90f4583ee053dfa9fa551ba81a59ef731e
tracey brimage <onelovenilynn@gmail.com>
2019-09-10 03:27:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,11 +5,10 @@ end
   bouroughs_in_nyc.unshift("Staten_island")
 end
 
-
-
-  bouroughs_in_nyc.pop
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
 end
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  
   bouroughs_in_nyc.pop_with_args
 end
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
