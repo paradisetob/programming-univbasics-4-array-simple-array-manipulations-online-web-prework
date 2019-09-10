5f00fbce44e645d88758624f97e6bb371e74e0d6
tracey brimage <onelovenilynn@gmail.com>
2019-09-10 03:42:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,7 @@
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   colors_in_the_rainbow.push("violet")
 end
+
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.unshift("Staten_island")
 end
@@ -8,16 +9,23 @@ end
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   continents.pop
 end
+
     continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
     continents.pop_with_args
 end
+
  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   my_favorite_cities.shift("Lagos")
 end
+
     ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   ice_cream_brands.shift_with_args("Blue Bell Creameries", "Ben & Jerry's")
 end
 
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 my_favorite_things.concat("sports cars","flat iron school")
+end
+
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+list_of_programming_languages.insert("python")
 end
